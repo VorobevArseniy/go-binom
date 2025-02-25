8c9be10f9a88bbd62bef3fd491fdcf049a553bec
VorobevArseniy <dr.venitus@gmail.com>
2025-02-26 00:18:43 +0500

    commit

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 )
 
 func main() {
@@ -12,23 +13,47 @@ func main() {
 	}
 
 	fmt.Println(inputs)
+
+	fmt.Println("Your binom: ", newtonBinom(inputs[0], inputs[1], inputs[2]))
+
 }
 
-// func makeBinom()
+func newtonBinom(a float64, b float64, n float64) float64 {
+	var x, k float64
 
-func input() ([]string, error) {
+	for ; k <= n; k++ {
+		x += combination(n, k) * math.Pow(a, n-k) * math.Pow(b, k)
+	}
 
-	var letter1, letter2, power string
-	res := make([]string, 0, 3)
+	return x
+}
+
+func factorial(n float64) float64 {
+	if n == 0 {
+		return 1
+	}
+
+	return n * factorial(n-1)
+}
+
+func combination(n float64, k float64) float64 {
+	c := factorial(n) / (factorial(k) * factorial(n-k))
+	return c
+}
+
+func input() ([]float64, error) {
+
+	var num1, num2, power float64
+	res := make([]float64, 0, 3)
 
 	fmt.Println("input first letter: ")
-	_, err := fmt.Scanln(&letter1)
+	_, err := fmt.Scanln(&num1)
 	if err != nil {
 		return nil, err
 	}
 
 	fmt.Println("input second letter: ")
-	_, err = fmt.Scanln(&letter2)
+	_, err = fmt.Scanln(&num2)
 	if err != nil {
 		return nil, err
 	}
@@ -39,7 +64,7 @@ func input() ([]string, error) {
 		return nil, err
 	}
 
-	res = append(res, letter1, letter2, power)
+	res = append(res, num1, num2, power)
 
 	return res, nil
 }
